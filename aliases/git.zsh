b8520918f321c3d29f655083abd9123e40ac0816
DanielSN <dan.san.nasc@gmail.com>
2023-12-19 14:41:39 -0300

    wip

--- aliases/git.zsh
+++ aliases/git.zsh
@@ -3,7 +3,6 @@
 alias gs='git status'
 alias gl='git log'
 alias gc='git commit -m'
-alias gca='git commit -am'
 alias ga='git add'
 alias gaa='git add .'
 alias g-wip='gaa && gc "wip"' 
@@ -14,4 +13,21 @@ alias g-wip='gaa && gc "wip"'
 
 function commit {
 	gaa && gc $1
+}
+
+function cbranch {
+	branch = $1
+	git checkout branch
+	if [ $? -eq 0 ]; then
+		echo "checkout $1"
+	else
+		echo "Branch não existe, deseja criar y/n"
+		read "Do you wish to install this program? " yn
+		case $yn in
+        	[Yy]* ) echo "yes";;
+        	[Nn]* ) exit;;
+        	* ) exit;;
+		esac
+	
+	fi
 }
